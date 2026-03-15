8f8f244215f9890eb198757351fc248950e8e8ba
Fabio Pittaccio <pittacciofabio@proton.me>
2026-03-15 18:29:31 +0100

    feat: add LocationTrigger actor
    Introduce ALocationTrigger:
    - adding header and implementation
    - Blueprint asset.
    
    The actor derives from ATriggerBox, exposes a UTextRenderComponent as TextComponent, and its constructor disables Tick and creates/attaches the TextComponent to the root.

---
+++ Source/StateTrees/Private/LocationTrigger.cpp
@@ -0,0 +1,14 @@
+﻿// Code written by Fabio Pittaccio - 2026
+
+#include "LocationTrigger.h"
+
+// Sets default values
+ALocationTrigger::ALocationTrigger()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	
+	// Create the text and attach the root component
+	TextComponent = CreateDefaultSubobject<UTextRenderComponent>("Text");
+	TextComponent->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+}
---
+++ Source/StateTrees/Public/LocationTrigger.h
@@ -0,0 +1,22 @@
+﻿// Code written by Fabio Pittaccio - 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/TextRenderComponent.h"
+#include "Engine/TriggerBox.h"
+#include "LocationTrigger.generated.h"
+
+UCLASS()
+class STATETREES_API ALocationTrigger : public ATriggerBox
+{
+	GENERATED_BODY()
+	
+public:
+	// Text Render Component
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "LocationTrigger")
+	UTextRenderComponent* TextComponent;
+	
+	// Constructor
+	ALocationTrigger();
+};
